130352f3970da5c30c7a46fbfb1abb9ff6aa85f6
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-07-03 20:01:54 +0530

    created public and private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import Sign from './pages/Sign';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/signin">SignIn</Route>
-        <Route path="/">Home</Route>
+        <Route path="/signin">
+        <Sign/>
+        </Route>
+
+        <PrivateRoute  path="/">
+        <Home/>
+        </PrivateRoute>
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/Sign.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sign() {
+  return (
+    <div>
+      signin page
+    </div>
+  )
+}
+
+export default Sign
