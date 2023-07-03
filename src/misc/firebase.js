1d5221e81fa435825bbcb7cafd3b6e414c79e35b
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-07-03 11:30:02 +0530

    thank your

---
+++ src/misc/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+
+
+
+const config = {
+  apiKey: "AIzaSyAlGjLBZOgbeRcGIWQGsZMT2PhBuKiOG7g",
+  authDomain: "chat-web-app-6a9dc.firebaseapp.com",
+  databaseURL: "https://chat-web-app-6a9dc-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-6a9dc",
+  storageBucket: "chat-web-app-6a9dc.appspot.com",
+  messagingSenderId: "355721514831",
+  appId: "1:355721514831:web:252aaf7f6fd9b454afff3a",
+  measurementId: "G-18SGX75ZQY"
+};
+
+
+firebase.initializeApp(config);
